d4c4c19677488a23b2b43bd70111f4cfe5d1daee
Isabelcasmot <isabelcasmot@gmail.com>
2022-05-21 18:47:35 +0100

    Fixed Form

--- js/index.js
+++ js/index.js
@@ -1,12 +1,69 @@
+// Get Data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error")
+
+
+// Validate data
+
+function validateForm() {
+
+    clearMessages();
+
+    let errorFlag = false;
+
+    if (nameInput.value.length < 1) {
+        errorNodes[0].innerText = "Name cannot be black";
+        nameInput.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if (!emailIsValid(email.value)) {
+        errorNodes[1].innerText = "Invalid email address";
+        email.classList.add("error-border");
+        errorFlag = true;
+
+    }
+
+    if (phone.value.length < 1) {
+        errorNodes[2].innerText = "Please enter a correct telephone number";
+        phone.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if (!errorFlag) {
+        success.innerText = "Success!";
+    }
+}
+
+// Clear error / success 
+
+function clearMessages() {
+    for (let i = 0; i < errorNodes.length; i++) {
+        errorNodes[i].innerText = "";
+    }
+
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    phone.classList.remove("error-border");
+
+}
+
+
+// Check if email is valid
+
+function emailIsValid(email) {
+    let pattern = /\S+@\S+\S+/;
+    return pattern.test(email);
+}
 
 
 
 
-
-
-
-
-
+// COUNTDOWN
 
 simplyCountdown('.count', {
     year: 2022,
